af2ade86a45074200a2530c01eb7f0cb989ce85b
muji <freeformsystems@gmail.com>
2016-03-13 13:08:35 +0800

    Draft buffer logic.

--- index.js
+++ index.js
@@ -1,32 +1,93 @@
-var assert = require('assert')
-  , commonmark = require('commonmark')
-  , through = require('through3')
+//var commonmark = require('commonmark')
+var through = require('through3')
   , fs = require('fs');
 
-function transform(chunk, encoding, cb){
-  assert(Array.isArray(chunk), 'cat: expects array of files');
-
-  var reader = new commonmark.Parser()
-    , doc;
-
-  chunk.forEach(function(file) {
-    var contents = fs.readFileSync(file)
-      , ast = reader.parse('' + contents);
-    if(!doc) {
-      doc = ast;
-    }else{
-      var walker = ast.walker();
-      var event, node;
-      while ((event = walker.next())) {
-        node = event.node;
-        if(event.entering && node.parent === ast) {
-          doc.appendChild(node);
-        }
-      }
-    }
+function read(chunk, encoding, cb) {
+  return fs.readFile(chunk, function(err, buf) {
+    cb(err, buf);
   })
-  this.push(doc);
+}
+
+function concat(chunk, encoding, cb) {
+  var scope = this
+    , files;
+
+  function next() {
+    var file = files.shift(); 
+    if(!file) {
+      return cb();
+    }
+
+    read(file, encoding, function(err, buf) {
+      if(err) {
+        return cb(err); 
+      }
+      scope.push(buf); 
+      next();
+    })
+  }
+
+  // pass through stdin
+  if(Buffer.isBuffer(chunk)) {
+    this.push(chunk); 
+    return cb();
+  // send on file chunks to be read
+  }else if(Array.isArray(chunk)) {
+    files = chunk.slice();
+    return next();
+  }
   cb();
 }
 
-module.exports = through.transform(transform);
+function cat(opts, cb) {
+  opts = opts || {};
+
+  var input = opts.input !== undefined ? opts.input : process.stdin
+    , files = opts.files || []
+    , called = false
+    , output = new cat.Concat()
+    , buf = new cat.Buffer();
+
+  function done(err) {
+    if(!called) {
+      return cb(err); 
+    } 
+    called = true;
+  }
+
+  if(input) {
+    input.once('error', done);
+  }
+
+  output.pipe(buf);
+
+  if(files) {
+    output.end(files); 
+  }
+
+  buf.once('finish', function() {
+    cb(null, buf.buffer); 
+  });
+
+  return output;
+}
+
+function BufferedReader() {
+  this.buffer = new Buffer(0);
+}
+
+function readChunk(chunk, encoding, cb) {
+  this.buffer = Buffer.concat(
+    [this.buffer, chunk], this.buffer.length + chunk.length);
+  cb();
+}
+
+function flush(cb) {
+  this.push(this.buffer);
+  cb();
+}
+
+cat.Concat = through.transform(concat);
+cat.Buffer = through.transform(readChunk, flush, {ctor: BufferedReader});
+
+module.exports = cat;
--- test/spec/cat.js
+++ test/spec/cat.js
@@ -1,22 +1,15 @@
 var expect = require('chai').expect
-  , mkcat = require('../../index');
+  , cat = require('../../index');
 
 describe('mkcat:', function() {
 
-  it('should cat files to ast', function(done) {
-    var concat = mkcat();
-    concat.on('data', function onData(data) {
-      expect(data.type).to.eql('Document');
-      expect(data.firstChild.firstChild.literal)
-        .to.eql('Heading (1)');
-      expect(data.lastChild.firstChild.literal)
-        .to.eql('Heading (2)');
-    })
-    concat.on('finish', function onFinish() {
-      done();
-    })
-    concat.end(
-      ['test/fixtures/heading1.md', 'test/fixtures/heading2.md']);
+  it('should cat files to buffer', function(done) {
+    cat(
+      {files: ['test/fixtures/heading1.md', 'test/fixtures/heading2.md']},
+      function(err, buf) {
+        expect(buf).to.be.an('object');
+        done(err); 
+      });
   });
 
 });
